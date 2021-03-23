1c620cb24273d4be09f73748886be35e9879e85a
hermanke <hermanke@tencent.com>
2021-03-23 16:26:59 +0800

    fix: eslint plugin order

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    new ReactRefreshPlugin(),
     {
       ...eslint({
         fix: true,
@@ -21,6 +20,7 @@ export default defineConfig({
       }),
       enforce: "pre", // 需要配置，否则会编译源文件
     },
+    new ReactRefreshPlugin(), // 这个插件的顺序只能在eslint后面，否则会导致eslint编译文件并且会出现重复定义的错误，应该是一个bug
   ],
   server: {
     port: 8000,
